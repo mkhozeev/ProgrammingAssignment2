ddb705857ad847ffe4cec82db3963764f77b40be
mkhozeev <mkhozeev@gmail.com>
2014-05-22 22:00:17 +0400

    Code I've generate to programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+#  Here is my second programming assignment version. I'd try to keep
+#  code as simply as I can and comply necessary requirements. It needs
+#  to inverse a matrix and return result or cached result
+##
 
-## Write a short comment describing this function
+## makeCacheMatrix implements methods to operate with matrixes
+#  using example:
+#  mt <- matrix(c(100,1,1,100),nrow=2,ncol=2)   # init a matrix
+# > mt
+# [,1] [,2]
+# [1,]  100    1
+# [2,]    1  100
+##
+#  mm <- makeCacheMatrix(mt)                    # create like obgect with methods
+##
+#  mm$get()                                     # use method "get" to view martix 
+# [,1] [,2]
+# [1,]  100    1
+# [2,]    1  100
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## makeCacheMatrix implements operations with @cache@
+#  using example:
+#  cacheSolve(mm)               # first use
+#            [,1]        [,2]
+# [1,]  0.01000100 -0.00010001
+# [2,] -0.00010001  0.01000100
+#  cacheSolve(mm)               # second use
+# getting cached matrix         # cached message appears
+# [,1]        [,2]
+# [1,]  0.01000100 -0.00010001
+# [2,] -0.00010001  0.01000100
+##
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  x$setsolve(s)
+  s
 }
